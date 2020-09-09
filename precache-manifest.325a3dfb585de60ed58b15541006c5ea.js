9e19d0d5d9faa286e45094836855fc9fe936df57
Sodiq <sodiq.akanmu001@gmail.com>
2020-09-09 13:20:08 +0100

    Updates

---
+++ precache-manifest.325a3dfb585de60ed58b15541006c5ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d91757f2df2c9f752dcdb0a95dac7e6",
+    "url": "/login005/index.html"
+  },
+  {
+    "revision": "f922937d49f7ec57aa4f",
+    "url": "/login005/static/css/main.03761bbb.chunk.css"
+  },
+  {
+    "revision": "5c3cba9c529e2ab7e9b4",
+    "url": "/login005/static/js/2.944f5c53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/login005/static/js/2.944f5c53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f922937d49f7ec57aa4f",
+    "url": "/login005/static/js/main.41410fe9.chunk.js"
+  },
+  {
+    "revision": "b6b427f375067825515f",
+    "url": "/login005/static/js/runtime-main.c575687a.js"
+  },
+  {
+    "revision": "f098eaeaf8868669382281f782d33871",
+    "url": "/login005/static/media/hamburger001.f098eaea.png"
+  }
+]);
